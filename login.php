5d0024fec3986e5bdbd9f71cdf1ef031134d2f8b
timeo-grrd <timeog53@gmail.com>
2025-11-25 14:51:56 +0100

    ajout de la fonctionnalité login ainsi que son interface visuel avec html

---
+++ login.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+require "fonctions.php";
+
+$pdo = getDB();
+
+if ($_SERVER['REQUEST_METHOD'] ==='POST'){
+    $email = trim ($_POST['email']);
+    $password = trim ($_POST['password']);
+
+    if ($email ===""|| $password ===""){
+        die ("Veuillez remplir tous les champs");
+    }
+
+    $user = getUserByEmail ($pdo, $email);
+
+    if (!$user){
+        die ("Email ou mot de passe incorect.");
+    }
+
+    if (!password_verify($password, $user ['password'])){
+        die("Email ou mot de passe incorrect.");
+    }
+
+    $_SESSION['user_id'] = $user ['id'];
+    $_SESSION['user_nom'] = $user ['nom'];
+
+    header("Location: tableau.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <header>
+    <nav class="nav-links">
+        <a href="index.html">Accueil</a>
+        <a href="register.php">Inscription</a>
+        <a href="login.php">Connexion</a>
+    </nav>
+    </header>
+
+    <h3>Accéder à ton compte</h3>
+    <form method="POST">
+    <div class="field">
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" required>
+    </div>
+    <div class="field">
+        <label for="password">Mot de passe</label>
+        <input id="password" type="password" name="password" required>
+    </div>
+    <div class="actions">
+        <button class="btn" type="submit">Se connecter</button>
+        <a class="inline-link" href="register.php">Créer un compte</a>
+    </div>
+    </form>  
+</body>
+</html>
